1b2ad738a20e8dc0e4bb03f45ac222975e793238
Holdest White <zbsholdest@mail.ru>
2024-05-12 03:03:35 +0300

    wore workers

--- tests/PlayWright/playwright-ct.config.ts
+++ tests/PlayWright/playwright-ct.config.ts
@@ -20,8 +20,7 @@ export default defineConfig<PlaywrightTestConfig>({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: 5,
-  // workers: 10,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['github'], ['line'], ['html']] : [['line'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -37,6 +36,7 @@ export default defineConfig<PlaywrightTestConfig>({
     ctViteConfig: {
       plugins: [react()],
       build: {
+        target: "es5",
         // it actually frees some memory that vite needs a lot
         // https://github.com/storybookjs/builder-vite/issues/409#issuecomment-1152848986
         sourcemap: false,
