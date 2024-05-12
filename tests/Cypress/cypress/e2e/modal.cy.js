581b6f222942a6663705c1126d8d98d78710df88
Holdest White <zbsholdest@mail.ru>
2024-05-12 15:30:49 +0300

    fix flow

--- tests/Cypress/cypress/e2e/modal.cy.js
+++ tests/Cypress/cypress/e2e/modal.cy.js
@@ -11,10 +11,10 @@ describe('modal', () => {
         cy.getByTestId('modal-body').should("contain", 'Woohoo, you are reading this text in a modal!')
         cy.getByTestId('modal-title').should("contain", 'Modal heading')
     })
-
-    it('Open button unreachable after modal open', () => {
-        cy.getByTestId('modal-open').click();
-
-        cy.getByTestId('modal-open').click();
-    })
+    //
+    // it('Open button unreachable after modal open', () => {
+    //     cy.getByTestId('modal-open').click();
+    //
+    //     cy.getByTestId('modal-open').click();
+    // })
 })
