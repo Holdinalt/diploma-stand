5f2b2c48f3c4d6c9a9c44567cbb112f7798c3b40
Holdest White <zbsholdest@mail.ru>
2024-05-13 19:14:03 +0300

    cleanup

--- tests/Cypress/cypress/e2e/navigation.cy.js
+++ tests/Cypress/cypress/e2e/navigation.cy.js
@@ -8,26 +8,9 @@ describe('navigation', () => {
     it('Check Button 1', () => {
         cy.getByTestId('button-1').click()
 
-        // cy.url().then(urlString => {
-        //     expect(urlString).include(VALID_URL)
-        // })
-
-        // cy.location('pathname', {timeout: 10000}).should('include', VALID_URL)
-
-        // cy.location().should(location => {
-        //     location.origin.startsWith(VALID_URL)
-        // })
-
-        // expect(cy.).to.eq(VALID_URL)
-
         cy.url().should('include', VALID_URL)
     })
 
-    // it('Check Button 2', () => {
-    //     cy.getByTestId('button-2').click()
-    //
-    //     cy.url().should('include', VALID_URL)
-    // })
 
     it('Check Link 1', () => {
         cy.getByTestId('link-1').should('have.attr', 'href').and('include', VALID_URL)
--- tests/Testcafe/tests/carousel.ts
+++ tests/Testcafe/tests/carousel.ts
@@ -18,21 +18,3 @@ test('cart is empty', async t => {
     await t.click(nextItem)
     await t.expect(await Selector('[data-testid="carousel-item-3"]').visible).ok()
 })
-
-// test.beforeEach(async ({ page }) => {
-//     await page.goto(`${standURL}/carousel`);
-// })
-//
-// test('carousel check', async ({ page }) => {
-//     const nextItem = page.locator('[class="carousel-control-next-icon"]')
-//
-//     await expect(page.getByTestId('carousel-item-1')).toBeVisible()
-//     await nextItem.click()
-//
-//     await page.waitForTimeout(1000);
-//     await expect(page.getByTestId('carousel-item-2')).toBeVisible()
-//     await nextItem.click()
-//
-//     await page.waitForTimeout(1000);
-//     await expect(page.getByTestId('carousel-item-3')).toBeVisible()
-// });
